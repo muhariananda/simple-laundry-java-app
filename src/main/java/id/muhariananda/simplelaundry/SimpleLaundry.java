14aa99d96a55ee07b45e827edd3d0884c74bed32
Muhammad Ariananda <muharianandakd@gmail.com>
2025-01-08 14:54:05 +0800

    initial commit

---
+++ src/main/java/id/muhariananda/simplelaundry/SimpleLaundry.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.muhariananda.simplelaundry;
+
+/**
+ *
+ * @author muhariananda
+ */
+public class SimpleLaundry {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
